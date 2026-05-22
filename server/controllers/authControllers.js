b0acf1f705992dfa09c7ba0a09bc21707916bd07
Meegy-exe <alison.dehaies@epitech.eu>
2026-05-22 23:10:39 +0200

    feat: add login in auth controller with jwt

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -3,6 +3,7 @@
 // import
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // route dautorisation de creation de compte
 exports.register = async (req, res) => {
@@ -15,11 +16,11 @@ exports.register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         // standard logique: est ce que le login/mail est deja inscrit
-        const userAlreadyExists = await User.findOne({ $or: [{ email }, { login } ]});
+        const userAlreadyExists = await User.findOne({ $or: [{ email }, { login }] });
         // SIL le login/mail est deja pris alors
         if (userAlreadyExists) {
             // return error 400 et message a luser
-            return res.status(400).json({ message: "Cet email ou ce login est déjà utilisé."});
+            return res.status(400).json({ message: "Cet email ou ce login est déjà utilisé." });
         }
 
         // standard logique: les mdps doivent correspondre
@@ -38,11 +39,57 @@ exports.register = async (req, res) => {
         await newUser.save();
 
         // return validation user
-        res.status(201).json({ message: "Votre compte a bien été créé."})
+        res.status(201).json({ message: "Votre compte a bien été créé." })
 
         // en cas d'erruer
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Erreur serveur au moment de l'inscription." });
+    }
+};
+
+
+// route dautorisation de connexion
+exports.login = async (req, res) => {
+    try {
+        // cible les datas recues du form react
+        const { login, password } = req.body;
+
+        // standard logique: est ce que le login est bien enregistré
+        const user = await User.findOne({ login });
+        // SIL nexiste pas alors
+        if (!user) {
+            // return error 404 et message a luser
+            return res.status(404).json({ message: "Ce compte n'existe pas." });
+        }
+
+        // standard logique: les mdps doivent correspondre avec celui hashé
+        // cible et compare les mdps
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        // SILS ne correspondent pas alors
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Votre mot de passe est incorrect." });
+        }
+
+        // jwt: token de securité
+        // expiration du token en 1h
+        const token = jwt.sign({
+            id: user._id, type: user.type
+        },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        // return message validation user avec token
+        res.status(200).json({
+            message: "Connexion réussie.",
+            token,
+            user: { login: user.login, type: user.type }
+        });
+
+        // en cas d'erreur
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur au moment de la connexion." });
     }
 };
